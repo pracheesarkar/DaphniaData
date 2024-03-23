9a23400864498e4d4c876f160d6b1030e70f4549
pracheesarkar <pracheesarkar@Prachees-Air.lan>
2024-03-22 21:16:09 -0400

    Updated main.py and refactored code

--- daphniaData/main.py
+++ daphniaData/main.py
@@ -1,86 +1,63 @@
+from typing import Any
+
 import rpy2
 import rpy2.robjects as robjects
+from pandas import Series, DataFrame
 from rpy2.robjects.packages import importr
+from geojson import MultiPoint, Feature, FeatureCollection, dumps
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import os
-
-base = importr('base')
-utils = importr('utils')
-stats = importr('stats')
+from calculations import Calculations
 
 # suppress R warnings
 from rpy2.rinterface_lib.callbacks import logger as rpy2_logger
 import logging
+
+base = importr('base')
+utils = importr('utils')
+stats = importr('stats')
 
 rpy2_logger.setLevel(logging.ERROR)
 
 # utils.install_packages('neonUtilities', repos='https://cran.rstudio.com/')
 neonUtilities = importr('neonUtilities')
 
-# read and merge zoo_taxonomyProcessed.csv and zoo_fieldData.csv
-data = pd.read_csv('/Users/pracheesarkar/Documents/AlvaradoLab/NEON_zooplankton/stackedFiles/zoo_taxonomyProcessed.csv')
-data2 = pd.read_csv('/Users/pracheesarkar/Documents/AlvaradoLab/NEON_zooplankton/stackedFiles/zoo_fieldData.csv')
-dataMerged = pd.merge(data, data2, on='sampleID')
+if __name__ == "__main__":
+    # read and merge zoo_taxonomyProcessed.csv and zoo_fieldData.csv
+    # edit this base_url variable to the folder where your csv files are.
+    # best practice would be to store the files in this repo/folder. just ignore them when we push to git
+    def get_data(daphnia=False):
+        # base_url = '/mnt/c/Users/nebul/Downloads/NEON_zooplankton 2/NEON_zooplankton/stackedFiles'
+        base_url = '/Users/pracheesarkar/Documents/AlvaradoLab/NEON_zooplankton/stackedFiles'
+        data = pd.read_csv("./data/zoo_taxonomyProcessed.csv")
+        data2 = pd.read_csv("./data/zoo_fieldData.csv")
+        data_merged = pd.merge(data, data2, on='sampleID')
+        data_daphnia = data_merged[data_merged.subfamily == 'Daphniidae']
 
-# filter data to only show Daphnia
-dataDaphnia = dataMerged[dataMerged.subfamily == 'Daphniidae']
-df = pd.DataFrame(dataDaphnia)
+        # filter data to only show Daphnia
+        if daphnia:
+            df = pd.DataFrame(data_daphnia)
+            df.to_csv("../data/daphnia.csv")
+        return data_daphnia
 
-# count total Daphnia collected in a site
-agg_functions = {'siteID_x': 'first', 'adjCountPerBottle': 'sum', }
-df_count = df.groupby(df['siteID_x']).aggregate(agg_functions)
+    # create dataframe with year and month separated for calculations
+    data_daphnia = get_data()
+    df = pd.DataFrame(data_daphnia)
+    df['collectDate_x'] = pd.to_datetime(df['collectDate_x'])
+    df['year'] = df['collectDate_x'].dt.year
+    df['month'] = df['collectDate_x'].dt.month
+    sites = ['BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK']
+    years =[2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+    desiredFolder = './figures'
 
-sites = ['BARC', 'CRAM', 'LIRO', 'PRLA', 'PRPO', 'SUGG', 'TOOK']
-site = df_count['siteID_x']
-count = df_count['adjCountPerBottle']
+    Calculations.site_count(df)
+    for i in range(len(sites)):
+        Calculations.yearly_count(sites[i], df)
+        for j in range(len(years)):
+            Calculations.daphnia_size_monthly(sites[i], years[j], df)
+            Calculations.daphnia_count_yearly(sites[i], years[j], df)
 
-desiredFolder = '/Users/pracheesarkar/Documents/AlvaradoLab/Daphnia_Data/Figures'
 
-# plot Daphnia count per site
-fig0 = plt.figure()
-plt.bar(site, count, color='lightblue')
-plt.xlabel("Sites")
-plt.ylabel("Count")
-plt.title("Daphnia Count Per Site ")
 
-plotpath = os.path.join(desiredFolder, "DataPerSite/CountPerSite.png")
-fig0.savefig(plotpath)
-plt.close()
 
-# plot Daphnia count and size per year in each site
-for i in range(len(sites)):
-    dataSite = dataDaphnia[dataDaphnia.siteID_x == sites[i]]
-    df2 = pd.DataFrame(dataSite)
-    df2['collectDate_x'] = pd.to_datetime(df2['collectDate_x'])
-    df2['year'] = df2['collectDate_x'].dt.year
-
-    #agg_samples = {'sampleID': 'first', 'adjCountPerBottle': 'sum'}
-    #total = {'sampleID': 'first', 'towerCountPerBottle': 'sum'}
-    agg_function = {'year': 'first', 'adjCountPerBottle': 'sum'}
-    agg_function2 = {'year': 'first', 'zooMeanLength': 'mean'}
-    df_count = df2.groupby(df2['year']).aggregate(agg_function)
-    df_size = df2.groupby(df2['year']).aggregate(agg_function2)
-
-    fig1 = plt.figure()
-    plt.bar(df_count['year'], df_count['adjCountPerBottle'], color='blue')
-    plt.xlabel("Year")
-    plt.ylabel("Count")
-    plt.title("Daphnia Count Per Year in Site " + sites[i])
-
-    plotpath = os.path.join(desiredFolder, "Count/{}.png".format(sites[i]))
-    fig1.savefig(plotpath)
-    plt.close()
-
-    fig2 = plt.figure()
-    plt.bar(df_size['year'], df_size['zooMeanLength'], color='lightgreen')
-    plt.xlabel("Year")
-    plt.ylabel("Size")
-    plt.ylim(0.00, 2.20)
-    plt.title("Daphnia Size Per Year in Site " + sites[i])
-
-    plotpath = os.path.join(desiredFolder, "Size/{}.png".format(sites[i]))
-    fig2.savefig(plotpath)
-    plt.close()
 
